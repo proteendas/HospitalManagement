80e48fa0a4ebc0022feb94884a0d7354fc3f8bee
Proteen Das <83655913+proteendas@users.noreply.github.com>
2023-04-20 15:21:09 +0530

    Initial Commit

--- dbconnection.php
+++ dbconnection.php
@@ -1,6 +1,6 @@
 <?php
 // Create connection
-$con=mysqli_connect("localhost","root","","ohmsphp");
+$con=mysqli_connect("localhost","root","","hms");
 
 // Check connection
 if (mysqli_connect_errno($con))
